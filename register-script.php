50186084133247c9663b423927aa2e7cea353250
Franz96 <francesco_liberatoscioli@hotmail.it>
2021-02-15 14:52:38 +0100

    Ho modifcato il codice in register-script ed in register

--- register-script.php
+++ register-script.php
@@ -1,11 +1,29 @@
 <?php
 /*  Leggere tutti i campi mandati dal utente    */
-$nome = $_POST['nome'];
-//$cognome = ....
-//$username = ....
+$nome = $_POST["nome"];
+$cognome = $_POST["cognome"];
+$username = $_POST["username"];
 //.....
+$servername = "localhost";
+$dbusername = "fiori";
+$dbpassword = "BKGjofSOLmfAQLPEBKGjofSOLmfAQLPE1!-@";
+$database = "fiori";
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $database);
+//Connessione al database
 
-//Connessione la database
+$servername = "localhost";
+$dbusername = "fiori";
+$dbpassword = "BKGjofSOLmfAQLPEBKGjofSOLmfAQLPE1!-@";
+$database = "fiori";
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error."<br>");
+}
+echo "Connected successfully<br>";
 
 
 // INSERT NEL DATABASE
