9716dcaeb6b457640610ccf689ff9c1092dcaaf1
franz96 <francesco_liberatoscioli@hotmail.it>
2021-04-23 11:28:08 +0200

    Ti pusho il codice

--- compra.php
+++ compra.php
@@ -15,9 +15,18 @@ function get_id_utente_loggato($conn){
 
 }
     $id_utente = get_id_utente_loggato($conn);
-function get_id_carrello($conn){
+function get_id_carrello($conn,$id_utente)
+{
     $sql = "SELECT ID FROM `carrelli` WHERE id_proprietario=$id_utente";
     $query = $conn->query($sql);
     $result = $query->fetch_all();
     return $result[0][0];
 }
+$id_carrello=get_id_carrello($conn,$id_utente);
+echo $id_carrello;
+
+function inserisci_prodotto($conn,$id_prodotto,$id_carrello,$quantita){
+    $sql = "INSERT INTO `prodotti_carrelli` (`id_carrello`, `id_prodotto`, `quantita`) VALUES ( '$id_carrello', '$id_prodotto', '$quantita');";
+    $conn->query($sql);
+}
+inserisci_prodotto($conn, $id,$id_carrello,$quantita);
